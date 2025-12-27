50ee3e6f2584e97716309c1cc8023d83fb292a00
Menny Levinski <menny.levinski@secuditor.com>
2025-12-27 18:56:28 +0200

    Update light-net-scanner.py

--- light-net-scanner.py
+++ light-net-scanner.py
@@ -110,7 +110,7 @@ class Spinner:
         self.thread.join()
 
 # Default common ports to check quickly
-COMMON_PORTS = [21, 22, 23, 67, 68, 69, 80, 123, 53, 161, 389, 443, 445, 1433, 3389, 5900]
+COMMON_PORTS = [21, 22, 23, 67, 68, 69, 80, 53, 123, 137, 138, 139, 161, 389, 443, 445, 1433, 3389, 5900]
 
 # OS detection
 IS_WINDOWS = platform.system().lower().startswith("win")
